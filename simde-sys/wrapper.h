eab916b04662213f0dd96283e65432559e718356
CGQAQ <m.jason.liu@outlook.com>
2022-10-19 15:26:55 +0800

    that's it, archived

---
+++ simde-sys/wrapper.h
@@ -0,0 +1,18 @@
+#include "simde/simde/simde-align.h"
+#include "simde/simde/simde-arch.h"
+#include "simde/simde/simde-common.h"
+#include "simde/simde/simde-complex.h"
+#include "simde/simde/simde-constify.h"
+#include "simde/simde/simde-detect-clang.h"
+#include "simde/simde/simde-diagnostic.h"
+#include "simde/simde/simde-f16.h"
+#include "simde/simde/simde-features.h"
+#include "simde/simde/simde-math.h"
+
+
+#include "simde/simde/arm/neon.h"
+#include "simde/simde/arm/sve.h"
+
+#include "simde/simde/mips/msa.h"
+
+#include "simde/simde/wasm/simd128.h"
